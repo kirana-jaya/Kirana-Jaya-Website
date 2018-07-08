a40eb1ec47ab01e5086d7394b6ef8997bec1c71c
Yosef Benny Widyokarsono <fybwid@gmail.com>
2018-07-08 17:48:29 +0000

    Set up a cleaning strategy for RSpec tests with Database Cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  # Set up a cleaning strategy for RSpec tests
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 Shoulda::Matchers.configure do |config|
